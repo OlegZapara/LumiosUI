51b519521cb5f0686aebf9e9f7fede53d2ed98d3
Oleg Zapara <ole9zp@gmail.com>
2024-03-20 18:22:22 +0200

    queue user interface

--- src/app/queues/queue.tsx
+++ src/app/queues/queue.tsx
@@ -3,13 +3,9 @@
 import { CardBody } from "@/components/ui/3d-card";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { GripVertical, LogOut, MoreHorizontal } from "lucide-react";
+import { ToastAction } from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
+import { ArrowUpToLine, LogOut, Pin, PinOff, Shuffle, Trash2 } from "lucide-react";
 import { useEffect, useState } from "react";
 
 let data = [
@@ -53,35 +49,109 @@ interface QueueProps{
 }
 
 export default function Queue(props: QueueProps) {
-
-  const [users, setUsers] = useState<string[]>([])
+  const [users, setUsers] = useState<{users: string[], undo: string[]}>({
+    users: [],
+    undo: []
+  })
+  const [isPinned, setIsPinned] = useState<boolean>(false)
+  const { toast } = useToast();
 
   useEffect(() => {
-    setUsers(data.map(x => x.name).slice(0, Math.max(Math.floor(Math.random() * data.length), 10)))
+    const usersData = data.map(x => x.name).slice(0, Math.max(Math.floor(Math.random() * data.length), 10))
+    setUsers({
+      users: usersData,
+      undo: usersData,
+    })
   }, [])
 
   function removeUser(name: string){
     console.log(name + "is removed");
-    setUsers(users.filter(user => user != name));
+    setUsers({
+      users: users.users.filter(user => user != name),
+      undo: users.users
+    })
+    toast({
+      title: props.title,
+      description: name + " was removed from queue",
+      duration: 3000,
+      action: (
+        <ToastAction altText="Undo deletion" onClick={() => undo()}>Undo</ToastAction>
+      )
+    });
+  }
+
+  function moveUp(name: string){
+    setUsers({
+      users: [name, ...users.users.filter(user => user != name)],
+      undo: users.users
+    })
+    toast({
+      title: props.title,
+      description: name + " is now first in the queue",
+      duration: 3000,
+      action: (
+        <ToastAction altText="Undo moving up" onClick={() => undo()}>Undo</ToastAction>
+      )
+    });
+  }
+
+  function undo(){
+    setUsers({
+      users: users.users,
+      undo: users.users
+    })
+  }
+
+  function shuffle(){
+    toast({
+      title: props.title + " was shuffled successfully",
+      action: (
+        <Shuffle className="stroke-green-500"></Shuffle>
+      ),
+      duration: 3000,
+    });
+  }
+
+  function deleteQueue(){
+    toast({
+      title: props.title + " was deleted successfully",
+      action: (
+        <Trash2 className="stroke-green-500"></Trash2>
+      ),
+      duration: 3000,
+    });
   }
 
   return (
     <Card className="flex justify-start [&>div]:w-full flex-col">
       <CardHeader>
-        <CardTitle><div>{props.title}</div></CardTitle>
+        <CardTitle className="flex flex-row justify-between items-center">
+          <div className="pl-4">{props.title}</div>
+          <div>
+            <Button variant="ghost" title="Shuffle queue" onClick={() => shuffle()}>
+              <Shuffle className="stroke-blue-500"></Shuffle>
+            </Button>
+            <Button variant="ghost" title={isPinned? "Unpin queue" : "Pin queue"} onClick={() => setIsPinned(p => !p)}>
+              {isPinned? <PinOff className="stroke-orange-300"></PinOff> : <Pin className="stroke-orange-300"></Pin>}
+            </Button>
+            <Button variant="ghost" title="Remove queue" onClick={() => deleteQueue()}>
+              <Trash2 className="stroke-red-500"></Trash2>
+            </Button>
+          </div>
+        </CardTitle>
       </CardHeader>
       <CardBody className="h-auto p-6 flex flex-col gap-1">
-        {users.map((user, i) => (
+        {users.users.map((user, i) => (
             <div
               key={user}
               className="group flex flex-row relative items-center justify-between h-8  border-transparent w-full"
             >
-              <GripVertical
-                className={`absolute hidden group-hover:flex cursor-move`}
-              ></GripVertical>
-              <div className="pl-8">{user}</div>
-              <Button variant="ghost" className={`h-8 w-8 p-0 hidden group-hover:flex`}>
-                <LogOut className="h-4 w-4 stroke-red-500" onClick={() => removeUser(user)}></LogOut>
+              <Button onClick={() => moveUp(user)} variant="ghost" className={`h-8 w-8 p-0 absolute hidden group-hover:flex`} aria-label="Move to start" title="Move to start">
+                <ArrowUpToLine className="h-4 w-4 stroke-green-500"></ArrowUpToLine>
+              </Button>
+              <div className="pl-10">{i + 1}. {user}</div>
+              <Button onClick={() => removeUser(user)} variant="ghost" className={`h-8 w-8 p-0 hidden group-hover:flex`} aria-label="Remove user" title="Remove user">
+                <LogOut className="h-4 w-4 stroke-red-500"></LogOut>
               </Button>
             </div>
           ))}
