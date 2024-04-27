079531e7fd695142f3a1fa4d963cb85244d676c4
Oleg Zapara <ole9zp@gmail.com>
2024-04-27 12:23:57 +0300

    settings fix

--- src/app/settings/timetable.tsx
+++ src/app/settings/timetable.tsx
@@ -1,10 +1,14 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-import useTimetableSettings from "@/hooks/useTimetableSettings";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Timetable() {
-  const [timetableSettings, setTimetableSettings] = useTimetableSettings();
+  const [chatId, setChatId] = useState<string>("");
+
+  useEffect(() => {
+    setChatId(localStorage.getItem("chatId") ?? "");
+  }, []);
 
   return (
     <div className="w-full flex flex-col gap-2">
@@ -20,15 +24,19 @@ export default function Timetable() {
       <section className="flex flex-col gap-2 my-4">
         <div className="leading-none tracking-tight">Chat ID</div>
         <div className="text-sm text-muted-foreground">Select your chat ID</div>
-        <Input
-          value={timetableSettings.chatId}
-          onChange={(e) =>
-            setTimetableSettings({
-              ...timetableSettings,
-              chatId: e.target.value,
-            })
-          }
-        ></Input>
+        <div className="flex flex-row w-full gap-2">
+          <Input
+            value={chatId}
+            onChange={(e) => setChatId(e.target.value)}
+          ></Input>
+          <Button
+            variant="outline"
+            className="px-8"
+            onClick={() => localStorage.setItem("chatId", chatId)}
+          >
+            Save
+          </Button>
+        </div>
       </section>
     </div>
   );
