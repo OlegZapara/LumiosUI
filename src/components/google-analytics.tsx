e4bba24b7c8832a8b00103176a23417de5d7589d
Oleg Zapara <ole9zp@gmail.com>
2024-05-28 18:54:14 +0300

    fix: Google Analytics

--- src/components/google-analytics.tsx
+++ src/components/google-analytics.tsx
@@ -6,7 +6,7 @@ const GoogleAnalytics = () => {
     <>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.MEASUREMENT_ID}`}
       />
 
       <Script id="" strategy="lazyOnload">
@@ -14,7 +14,7 @@ const GoogleAnalytics = () => {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+              gtag('config', '${process.env.MEASUREMENT_ID}', {
               page_path: window.location.pathname,
               });
           `}
